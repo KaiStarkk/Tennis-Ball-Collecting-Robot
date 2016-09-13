052774f8c4f143091cb23fdf5e9ebf18ec5f7277
Kieran Hannigan <overyonderstudios@gmail.com>
2016-09-13 12:09:45 +0800

    Fixed style

--- detect.py
+++ detect.py
@@ -22,34 +22,40 @@ minval = 50
 minsize = 200
 
 while display.isNotDone():
-	
-	blobs = []
-	splotches = []
-	circles = []
 
-	count = 0
+    blobs = []
+    splotches = []
+    circles = []
 
-	img = cam.getImage().flipHorizontal()
-	dist = img.hueDistance(hue,minsat,minval)
+    count = 0
 
-	thresh = dist.binarize(bound).erode(erode).dilate(dilate)
+    img = cam.getImage().flipHorizontal()
+    dist = img.hueDistance(hue, insat, inval)
 
-	blobs = thresh.findBlobs()
-	
-	if blobs:
-		splotches = blobs.filter([b.area() > minsize for b in blobs])
-	if splotches:
-		circles = splotches.filter([s.isCircle(roundness) for s in splotches])
-	for c in circles:
-		img.drawCircle((c.x, c.y), c.radius(),SimpleCV.Color.RED,3)
-		img.drawText(str(len(circles)-count), c.x, c.y,SimpleCV.Color.BLUE,int(c.radius())-10)
-		count += 1
+    thresh = dist.binarize(bound).erode(erode).dilate(dilate)
 
-	label = "Tennis Balls found: " + str(len(circles))
-	panel = DrawingLayer(img.size())
-	panel.polygon(((0,0),(0,60),(200,60),(200,0)),(100,100,100),0,True,True, 200)
+    blobs = thresh.findBlobs()
 
-	img.addDrawingLayer(panel)
-	img.drawText(label,15,20,SimpleCV.Color.WHITE,24)
+    if blobs:
+        splotches = blobs.filter([b.area() > minsize for b in blobs])
+    if splotches:
+        circles = splotches.filter([s.isCircle(roundness) for s in splotches])
+    for c in circles:
+        img.drawCircle((c.x, c.y), c.radius(), impleCV.Color.RED, 3)
+        img.drawText(str(len(circles) - count),
+                     c.x, c.y,
+                     impleCV.Color.BLUE,
+                     nt(c.radius()) - 10)
+        count += 1
 
-	img.show()
+    label = "Tennis Balls found: " + str(len(circles))
+    panel = DrawingLayer(img.size())
+    panel.polygon(((0, 0), (0, 60),
+                   (200, 60), (200, 0)
+                   ),
+                  (100, 100, 100), 0, True, True, 200)
+
+    img.addDrawingLayer(panel)
+    img.drawText(label, 15, 20, SimpleCV.Color.WHITE, 24)
+
+    img.show()
